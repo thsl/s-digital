afb4b76b19a38f772c3054d75868fc1462c67259
leonardo.braga <Leonardo.Braga@bancoob.br>
2017-03-03 14:35:39 -0300

    Adicionando arquivo server.js para subir um servidor (container web) com NodeJs

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+global.pathRootApp = path.resolve(__dirname);
+
+app.use(express.static(path.join(__dirname)));
+
+/* subindo servidor */
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log("Conectado na porta %s...", server.address().port);
+});
