c7274aa39cc3c14f9f99df89046445f963d5e21b
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2017-03-10 17:03:05 -0300

    Reajusta theme.js

--- js/theme.js
+++ js/theme.js
@@ -6,28 +6,22 @@ $(document).ready(function () {
 
     new WOW().init();
 // smooth scroller
-    /*
-     if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-     window.onmousewheel = document.onmousewheel = wheel;
 
-     function wheel(event) {
-     var delta = 0;
-     if (event.wheelDelta) delta = event.wheelDelta / 120;
-     else if (event.detail) delta = -event.detail / 3;
+    smooth();
 
-     handle(delta);
-     if (event.preventDefault) event.preventDefault();
-     event.returnValue = false;
-     }
+// Faz o back do browser fechar o modal
+    $(".modal").on("shown.bs.modal", function() { // any time a modal is shown
+        var urlReplace = "#" + $(this).attr('id'); // make the hash the id of the modal shown
+        history.pushState(null, null, urlReplace); // push state that hash into the url
 
-     function handle(delta) {
-     var time = 1000; // delay time
-     var distance = 300; // delta point
-     // Dom where it will apply
-     $('.modal').stop().animate({
-     scrollTop: $(window).scrollTop() - (distance * delta)
-     }, time);
-     }*/
+        smoothOff();
+    });
+
+    $(".modal").on("hidden", function() { // any time a modal is shown
+       console.log("desligando");
+
+        smoothOff();
+    });
 
     $('#modal-detalha').on('shown', function() {
         $("body").css("overflow", "hidden");
@@ -35,16 +29,13 @@ $(document).ready(function () {
 
     $('#modal-detalha').on('hidden', function() {
         $("body").css("overflow", "visible");
+        console.log("ligando");
+        smooth();
     });
 
     $('body').on('hidden.bs.modal', '.modal', function() {
         $(this).removeData('bs.modal');
-    });
-
-// Faz o back do browser fechar o modal
-    $(".modal").on("shown.bs.modal", function() { // any time a modal is shown
-        var urlReplace = "#" + $(this).attr('id'); // make the hash the id of the modal shown
-        history.pushState(null, null, urlReplace); // push state that hash into the url
+       smooth();
     });
 
 // If a pushstate has previously happened and the back button is clicked, hide any modals.
@@ -52,6 +43,48 @@ $(document).ready(function () {
         $(".modal").modal('hide');
     });
 
-
-
 });
+
+
+function smoothOff(){
+
+
+    window.addEventListener('DOMMouseScroll', function(event){}, false);
+    window.onmousewheel = document.onmousewheel = function(event){};
+}
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function smooth() {
+    if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+    window.onmousewheel = document.onmousewheel = wheel;
+
+    function wheel(event) {
+        var delta = 0;
+        if (event.wheelDelta) delta = event.wheelDelta / 120;
+        else if (event.detail) delta = -event.detail / 3;
+
+        handle(delta);
+        if (event.preventDefault) event.preventDefault();
+        event.returnValue = false;
+    }
+
+    function handle(delta) {
+        var time = 1000; // delay time
+        var distance = 300; // delta point
+        // Dom where it will apply
+        $('body').stop().animate({
+            scrollTop: $(window).scrollTop() - (distance * delta)
+        }, time);
+    }
+
+
+}
